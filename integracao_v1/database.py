41d08ba78652e3e0d3c44d8d7e827c33cf6ef9b4
Adalberto Carvalho <107323618+adalbertocarv@users.noreply.github.com>
2025-10-02 11:40:33 -0300

    novo bd

--- integracao_v1/database.py
+++ integracao_v1/database.py
@@ -5,16 +5,19 @@ def carregar_dados_bd(caminho_bd):
     conn = sqlite3.connect(caminho_bd)
     cursor = conn.cursor()
     
-    # Dicionários para armazenar as paradas e linhas
     paradas = []
     linhas_de_onibus = defaultdict(list)
     
     cursor.execute('SELECT id_ponto_parada, linhas FROM tab_linha_parada')
     for row in cursor.fetchall():
         id_ponto_parada = row[0]
-        linhas = row[1].split(', ')
+        linhas_str = row[1]
+        
+        if linhas_str:  # só divide se não for None ou vazio
+            linhas = linhas_str.split(', ')
+            linhas_de_onibus[id_ponto_parada].extend(linhas)
+        
         paradas.append(id_ponto_parada)
-        linhas_de_onibus[id_ponto_parada].extend(linhas)
     
     conn.close()
     return paradas, linhas_de_onibus
